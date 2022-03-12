d676b7d18a3c05f799c0512b6f94a28c568c8b1d
hugosenari <hugosenari@gmail.com>
2022-03-12 02:00:27 -0300

    chore: re index io-isense

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Obsolete - please use isense instead!'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-io-isense-master.flake = false;
+  inputs.src-io-isense-master.owner = "nimious";
+  inputs.src-io-isense-master.ref   = "refs/heads/master";
+  inputs.src-io-isense-master.repo  = "io-isense";
+  inputs.src-io-isense-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-io-isense-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-io-isense-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
